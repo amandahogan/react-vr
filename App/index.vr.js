17bb0e0f19d4fa33ed7a35e8019df661c1a7fd82
amandahogan <hogan.ans@gmail.com>
2017-08-23 15:17:30 +1200

    Add Moon wireframe Model

--- App/index.vr.js
+++ App/index.vr.js
@@ -5,25 +5,45 @@ import {
   Pano,
   View,
   Text,
+  Model,
+  AmbientLight,
+  DirectionalLight,
 } from 'react-vr'
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
-        <Pano 
+        {/* <Pano 
           source={asset('mohaka.jpg')}
           onLoad={() => { console.log('Loaded')} }
+        /> */}
+        <DirectionalLight 
+          style={{
+            transform: [{translateX: -1000}]
+          }}
         />
-        Test Text
-        <Text
+        <AmbientLight intensity={ 2.6 } />
+        <Model
+          source={{
+            obj: asset('moon.obj')
+          }}
+          style={{
+            transform: [
+              {translate: [0, -1, -2]},
+              {scale: 0.005},
+            ],
+          }}
+          wireframe
+        />
+        {/* <Text
           style = {{
             transform: [{translateZ: -1.25}],
             layoutOrigin: [0.5, 0.5]
           }}
         >
           Mangatutu Hot Springs
-        </Text>
+        </Text> */}
       </View>
     )
   }
