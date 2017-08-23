c6792ad6f502b23bc0a2c716b90f26158ef0b586
amandahogan <hogan.ans@gmail.com>
2017-08-23 14:03:57 +1200

    Complete Pano and Text

--- App/index.vr.js
+++ App/index.vr.js
@@ -1,34 +1,32 @@
-import React from 'react';
+import React from 'react'
 import {
   AppRegistry,
   asset,
   Pano,
-  Text,
   View,
-} from 'react-vr';
+  Text,
+} from 'react-vr'
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
+        <Pano 
+          source={asset('mohaka.jpg')}
+          onLoad={() => { console.log('Loaded')} }
+        />
+        Test Text
         <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
+          style = {{
+            transform: [{translateZ: -1.25}],
+            layoutOrigin: [0.5, 0.5]
+          }}
+        >
+          Mangatutu Hot Springs
         </Text>
       </View>
-    );
+    )
   }
-};
+}
 
-AppRegistry.registerComponent('App', () => App);
+AppRegistry.registerComponent('App', () => App)
