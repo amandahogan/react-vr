0336250df9b757c119a35654f3d55e1ef92763a8
amandahogan <hogan.ans@gmail.com>
2017-08-23 16:13:08 +1200

    add map location view

--- App/index.vr.js
+++ App/index.vr.js
@@ -8,6 +8,7 @@ import {
   Model,
   AmbientLight,
   DirectionalLight,
+  Image
 } from 'react-vr'
 
 export default class App extends React.Component {
@@ -46,6 +47,37 @@ export default class App extends React.Component {
         >
           Mangatutu Hot Springs
         </Text>
+        <View 
+          style={{
+            width: 2,
+            height: 2,
+            backgroundColor: '#FFF',
+            layoutOrigin: [0.5, 0.5],
+            transform: [
+              {translate: [2, 0, -3]}
+            ]
+          }}
+          onInput={(event) => {
+            console.log('type', event.nativeEvent.inputEvent.type)
+            console.log('eventType', event.nativeEvent.inputEvent.eventType)
+          }}
+        >
+          <Image 
+            source={asset('location.png')} 
+            style={{
+              height: 1.8
+            }}
+          />
+          <Text
+            style={{
+              color: '#333',
+              fontSize: 0.12,
+              textAlign: 'center'
+            }}
+          >
+            Click Here to find this hidden gem
+          </Text>
+        </View>
       </View>
     )
   }
